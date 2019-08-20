3c00fcc092732b14b23a012a6d9394af411110ea
Henry Cheng <account048924@gmail.com>
2019-08-20 17:12:52 +0800

    修改2

--- password-retry.py
+++ password-retry.py
@@ -8,17 +8,17 @@
 
 password = 'a123456'
 count_pwds = 3
-pwd = input('請輸入密碼： ')
+
 #pwd = int(pwd)
 while count_pwds > 0:
-
+	pwd = input('請輸入密碼： ')
 	if pwd == password:
 		print('登入成功!')
 		break
 	elif pwd != password and count_pwds > 1:
 		count_pwds = count_pwds - 1 
 		print("密碼錯誤! 還有",count_pwds,"次機會")
-		pwd = input('請輸入密碼： ')
+		#pwd = input('請輸入密碼： ')
 	else:
 		print('已錯誤三次無法登入!')
 		break	
